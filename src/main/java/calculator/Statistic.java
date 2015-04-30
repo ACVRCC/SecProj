c089dfc221b5d5f2854778b01824b4725a64ccac
ACVRCC <centenocosta@gmail.com>
2015-04-30 14:56:20 +0100

    alterações estatística

--- src/main/java/calculator/Statistic.java
+++ src/main/java/calculator/Statistic.java
@@ -11,9 +11,9 @@ public class Statistic implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	private int countPlus=0;
-	
-
-	
+	private int countMinus=0;
+	private int countTimes=0;
+	private int countDivide=0;
 
 
 	public void countOperator(String operator){
@@ -21,14 +21,15 @@ public class Statistic implements Serializable{
 		switch (operator){
 
 		case "plus": countPlus++;
+		case "minus": countMinus++;
+		case "times": countTimes++;
+		case "divide": countDivide++;
 				
-		break;
-		
+		break;		
 
 		}
 	
 	}
-	
 	
 
 	public int getCountPlus() {
@@ -38,6 +39,36 @@ public class Statistic implements Serializable{
 
 	public void setCountPlus(int countPlus) {
 		this.countPlus = countPlus;
+	}
+
+
+	public int getCountMinus() {
+		return countMinus;
+	}
+
+
+	public void setCountMinus(int countMinus) {
+		this.countMinus = countMinus;
+	}
+
+
+	public int getCountTimes() {
+		return countTimes;
+	}
+
+
+	public void setCountTimes(int countTimes) {
+		this.countTimes = countTimes;
+	}
+
+
+	public int getCountDivide() {
+		return countDivide;
+	}
+
+
+	public void setCountDivide(int countDivide) {
+		this.countDivide = countDivide;
 	}
 	
 
